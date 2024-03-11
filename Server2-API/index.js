3c0287eba51d8b14f6d7bd006b2f1f10ad13fc47
jim3 <jimmy3.xyz@gmail.com>
2024-03-11 10:37:22 -0400

    Update index.js

--- Server2-API/index.js
+++ Server2-API/index.js
@@ -1,3 +1,5 @@
+// Server #2 - Our live server is responsible for receiving sensor data
+// from the BME680 sensor [local server] and sending it to the client
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
@@ -19,12 +21,11 @@ app.get("/events", (req, res) => {
     res.setHeader("Content-Type", "text/event-stream"); // set the content type to event-stream
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Connection", "keep-alive");
-    res.setHeader("X-Accel-Buffering", "no"); // NGINX: unbuffered responses
+    res.setHeader("X-Accel-Buffering", "no"); // For NGINX unbuffered responses
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.flushHeaders(); // flush the headers to establish SSE with client
+    res.flushHeaders(); // flush the headers to establish SSE with the client
 
     clients.push(res);
-
     req.on("close", () => {
         clients = clients.filter((client) => client !== res);
     });
@@ -32,7 +33,7 @@ app.get("/events", (req, res) => {
 
 // this is our /sensors endpoint where we recieve our sensor data
 app.post("/sensors", (req, res) => {
-    console.log("Received sensor data:", req.body); // this logs the sensor data
+    console.log("Server #2 has received bme680 sensor data:", req.body); // this logs the sensor data
     clients.forEach((client) => client.write(`data: ${JSON.stringify(req.body)}\n\n`));
     res.status(204).end();
 });
